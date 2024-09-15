f0fc3362fe4b67bfcb955d9bf147b101cfc61180
roxeannechico <roseannechico22@gmail.com>
2024-09-15 11:14:18 +0800

    initial commit

---
+++ dataConversion.js
@@ -0,0 +1,31 @@
+// Convert a String to a Number
+let price = '200';
+
+//Three Different Methods:
+// Using parseInt()
+let priceAsInt = parseInt(price);
+console.log(priceAsInt);
+// Using Number()
+let priceAsNumber = Number(price);
+console.log(priceAsNumber); 
+// Using + operator
+let priceWithPlus = +price;
+console.log(priceWithPlus);
+
+// Convert a Number to a String
+let distance = '150';
+
+//Two Methods:
+// Using .toString()
+let distanceAsString = distance.toString();
+console.log(distanceAsString); 
+// Using String()
+let distanceWithString = String(distance);
+console.log(distanceWithString);
+
+// Convert a String to a Decimal Number
+let weight = '72.5';
+
+// Using parseFloat()
+let weightAsFloat = parseFloat(weight);
+console.log(weightAsFloat);
